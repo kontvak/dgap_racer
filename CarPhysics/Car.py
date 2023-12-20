f31d50c5025037be3cafe737ccf2f3acf984fa71
kontvak <kontvak@gmail.com>
2023-12-20 16:00:24 +0300

    final?

--- CarPhysics/Car.py
+++ CarPhysics/Car.py
@@ -1,16 +1,20 @@
-import math
+import ast
 import pygame
 from CarPhysics.geomery_methods import *
-import gym
-from gym import spaces
-from gym.utils import seeding
+from geomery_methods import *
 from shapely.geometry import Point
 from shapely.geometry.polygon import Polygon
 
 
+file = open('Assets/track0.txt', 'r')
+track_out = ast.literal_eval(next(file))
+track_in = ast.literal_eval(next(file))
+track_score = ast.literal_eval(next(file))
+file.close()
+
 class Car():
-    def __init__(self):
-        self.x, self.y = 180, 265
+    def __init__(self, start=(180, 265)):
+        self.x, self.y = start
         self.vel = 0
         self.direction = [1, 0]
         self.direction = rotate(self.direction, -5)
@@ -37,9 +41,10 @@ class Car():
         self.accelerating = False
         self.reversing = False
 
-
-
     def move(self):
+        """
+        Двигает машину
+        """
         multiplier = 1
         if abs(self.vel) < 5:
             multiplier = abs(self.vel) / 5
@@ -75,7 +80,6 @@ class Car():
             self.vel = self.maxSpeed
         elif self.vel < self.maxReverseSpeed:
             self.vel = self.maxReverseSpeed
-
         addVector = [0, 0]
 
         addVector[0] += self.vel * self.direction[0]
@@ -87,28 +91,19 @@ class Car():
         self.x += addVector[0]
         self.y += addVector[1]
 
-
     def draw(self, screen):
+        """
+        Отрисовка
+        :param screen: экран
+        """
         rotated_image = pygame.transform.rotate(self.carPic, -get_angle(self.direction) - 90)
         new_rect = rotated_image.get_rect(center=(self.x, self.y))
-
         screen.blit(rotated_image, new_rect)
 
-
-    def reset(self):
-        self.done = False
-        self.x, self.y = (180, 265)
-        self.vel = 0
-        self.direction = [1, 0]
-        self.acc = 0
-        self.reward = 0
-
-
-
-
     def not_cross_wall(self):
-        track_in = self.track_in
-        track_out = self.track_out
+        """
+        Проверяет пересечение со стенами
+        """
         polygon_in = Polygon(track_in)
         polygon_out = Polygon(track_out)
         for x, y in [(self.x + (self.width / 2 - 3) * self.direction[0] + (self.height / 2 - 3) * self.direction[1],
@@ -131,21 +126,21 @@ class Car():
         return True
 
 
-
-
-
-
-
-
 class UserCar(Car):
-    def __init__(self):
-        Car.__init__(self)
+    def __init__(self, start=False):
+        if not start:
+            Car.__init__(self)
+        else:
+            Car.__init__(self, start)
 
         self.carPic = pygame.image.load('Assets/usercar.png')
         self.carPic = pygame.transform.scale(self.carPic, (self.height, self.width))
 
-
     def updateWithUser(self, wasd_list):
+        """
+        Применяет двействия с клавиатуры
+        :param wasd_list: нажатие клавиш
+        """
         self.turningLeft = False
         self.turningRight = False
         self.accelerating = False
@@ -153,17 +148,20 @@ class UserCar(Car):
 
         (self.accelerating, self.turningLeft, self.reversing, self.turningRight) = wasd_list
 
-
+    def cross_wall_reaction(self):
+        """
+        Двействие в случае пересечения границы
+        """
+        if not self.not_cross_wall():
+            self.vel *= 0.97
 
 
 class BotCar(Car):
-    def __init__(self):
-        Car.__init__(self)
-
-
-        self.track_out = [(89, 260), (98, 156), (147, 107), (857, 85), (915, 113), (944, 183), (962, 578), (940, 621), (902, 638), (834, 614), (713, 462), (648, 450), (518, 478), (457, 597), (404, 626), (345, 625), (302, 584), (260, 349), (243, 312), (205, 307), (111, 304)]
-        self.track_in = [(151, 239), (154, 175), (169, 160), (828, 138), (868, 150), (891, 211), (915, 538), (908, 566), (865, 571), (793, 461), (703, 396), (590, 394), (503, 427), (422, 550), (396, 569), (365, 571), (335, 477), (316, 302), (269, 261)]
-        self.track_score = [(297, 119), (156, 127), (109, 160), (104, 250), (123, 290), (283, 288), (302, 320), (324, 589), (378, 610), (452, 583), (509, 439), (599, 412), (699, 412), (840, 602), (903, 622), (949, 580), (924, 177), (899, 121), (839, 94)]
+    def __init__(self, start=False):
+        if not start:
+            Car.__init__(self)
+        else:
+            Car.__init__(self, start)
 
         self.dead = False
         self.now_iteration = 0
@@ -174,20 +172,23 @@ class BotCar(Car):
         self.carPic = pygame.transform.scale(self.carPic, (self.height, self.width))
 
     def cross_wall_reaction(self):
+        """
+        Двействие в случае пересечения границы
+        """
         if not self.not_cross_wall():
             self.dead = True
 
     def step(self, action):
-
+        """
+        Примененяет action и обновляет положение и self.max_iteration для обучения
+        """
+        #global track_score
         self.updateWithAction(action)
         self.move()
-        #self.reward = score_distanse((self.x, self.y), self.track_score)[0] - 390
         self.cross_wall_reaction()
-        #state = self.get_state()
-        #return state #, self.reward, self.done, {}
 
         self.now_iteration += 1
-        now_score = (score_distanse((self.x, self.y), self.track_score)[0]) // 20 % 135
+        now_score = (score_distanse((self.x, self.y), track_score)[0]) // 20 % 135
         if now_score == 134:
             self.last_score = 0
         if 0 < now_score - self.last_score <= 5:
@@ -198,9 +199,9 @@ class BotCar(Car):
 
 
     def get_state(self):
-        track_in = self.track_in
-        track_out = self.track_out
-        #print(first_intersection((self.x, self.y), rotate(self.direction, 0.5), track_out, track_in))
+        """
+        Параметры для обучения
+        """
         d_0_1 = distanse((self.x, self.y), first_intersection((self.x, self.y), rotate(self.direction, 0.5), track_out, track_in))
         d_0_2 = distanse((self.x, self.y), first_intersection((self.x, self.y), rotate(self.direction, -0.5), track_out, track_in))
         tan_0 = 0.01745 * d_0_1 / (d_0_2 - d_0_1 + 0.00001)
@@ -218,6 +219,9 @@ class BotCar(Car):
 
 
     def updateWithAction(self, actionNo):
+        """
+        Применяет действие из его номера
+        """
         self.turningLeft = False
         self.turningRight = False
         self.accelerating = False
--- main.py
+++ main.py
@@ -1,19 +1,21 @@
-import pygame
-import time
-from shapely.geometry import Point
+import os
 import ast
-from shapely.geometry.polygon import Polygon
-from Car import *
+import pygame
 
-import pyglet
+import pickle
+import neat
+
+from CarPhysics.Car import *
+
 
 FPS = 60
 screen_width = 1080
 screen_height = 720
-BACK_COLOR = (0,0,0)
+BACK_COLOR = (0, 0, 0)
 TRACK_COLOR = (25, 25, 25)
+RED_COLOR = (205, 0, 55)
 
-file = open('track0.txt', 'r')
+file = open('Assets/track0.txt', 'r')
 track_out = ast.literal_eval(next(file))
 track_in = ast.literal_eval(next(file))
 track_score = ast.literal_eval(next(file))
@@ -24,37 +26,71 @@ pygame.init()
 pygame.display.set_caption("racist")
 clock = pygame.time.Clock()
 screen = pygame.display.set_mode((screen_width, screen_height), 0, 32)
-car1 = Car()
+car1 = BotCar()
+car2 = UserCar((180, 285))
+
+winner = pickle.load(open("learning/winner1.pkl", "rb"))
+local_dir = os.path.dirname(__file__)
+config_path = os.path.join(local_dir, 'learning/config.txt')
+config = neat.config.Config(
+    neat.DefaultGenome,
+    neat.DefaultReproduction,
+    neat.DefaultSpeciesSet,
+    neat.DefaultStagnation,
+    config_path
+)
+winner_net = neat.nn.FeedForwardNetwork.create(winner, config)
+
+clock = pygame.time.Clock()
+counter, text = 5, '5'.rjust(3)
+pygame.time.set_timer(pygame.USEREVENT, 500)
+font = pygame.font.SysFont('Consolas', 30)
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.USEREVENT:
+            counter -= 1
+            if counter > 0:
+                text = str(counter).rjust(3)
+            else:
+                run = False
+        if event.type == pygame.QUIT:
+            run = False
+
+    screen.fill(BACK_COLOR)
+    pygame.draw.polygon(screen, TRACK_COLOR, track_out)
+    pygame.draw.polygon(screen, BACK_COLOR, track_in)
+    pygame.draw.polygon(screen, RED_COLOR, track_out, 2)
+    pygame.draw.polygon(screen, RED_COLOR, track_in, 2)
+    car1.draw(screen)
+    car2.draw(screen)
+    screen.blit(font.render(text, True, (255, 255, 255)), (32, 48))
+    pygame.display.flip()
+    clock.tick(60)
+run = True
 while run:
     clock.tick(FPS)
     screen.fill(BACK_COLOR)
     pygame.draw.polygon(screen, TRACK_COLOR, track_out)
     pygame.draw.polygon(screen, BACK_COLOR, track_in)
+    pygame.draw.polygon(screen, RED_COLOR, track_out, 2)
+    pygame.draw.polygon(screen, RED_COLOR, track_in, 2)
     car1.draw(screen)
-
-    point_out = first_intersection((car1.x, car1.y), rotate(car1.direction, 0), track_in, track_out)
-    if point_out:
-        pygame.draw.circle(screen, (0, 0, 255), point_out, 2)
-
-    npoint = normal_point_to_polygon((car1.x, car1.y), track_score)[0]
-    pygame.draw.circle(screen, (0, 0, 255), (297, 119), 2)
-    if npoint:
-        pygame.draw.circle(screen, (0, 0, 255), npoint, 2)
-
-    print(score_distanse((car1.x, car1.y), track_score))
+    car2.draw(screen)
 
     keys = pygame.key.get_pressed()
     wasd = (keys[pygame.K_w], keys[pygame.K_d], keys[pygame.K_s], keys[pygame.K_a])
 
-    car1.updateWithUser(wasd)
-    car1.move()
+    car2.move()
+    car2.updateWithUser(wasd)
+    car2.cross_wall_reaction()
 
+    car1.move()
+    ac = winner_net.activate(car1.get_state())
+    car1.updateWithAction(max(enumerate(ac),key=lambda x: x[1])[0])
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
             break
-
-
-
     pygame.display.update()
 pygame.quit()
